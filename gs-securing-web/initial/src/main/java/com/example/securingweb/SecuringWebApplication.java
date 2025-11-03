5a06ea46fbd41c5c9fc6a0bd4410cc9cfe6c49e9
hant0505 <23020532@vnu.edu.vn>
2025-11-03 16:00:21 +0700

    switchh

--- gs-securing-web/initial/src/main/java/com/example/securingweb/SecuringWebApplication.java
+++ gs-securing-web/initial/src/main/java/com/example/securingweb/SecuringWebApplication.java
@@ -2,7 +2,9 @@ package com.example.securingweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;   // cần check đây
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 @SpringBootApplication
 public class SecuringWebApplication {
 
